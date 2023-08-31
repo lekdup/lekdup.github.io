3fea46065385598c25de905cf58f66874d762423
lekdup <lekdup@live.fr>
2023-08-31 17:54:41 +0200

    home done

--- my-portfolio/src/components/App.js
+++ my-portfolio/src/components/App.js
@@ -1,11 +1,11 @@
 import './App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faCode, faDownload, faEnvelope, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faCode, faEnvelope, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
-import About from './About/About';
+import Home from './Home/Home';
 import NavBar from './NavBar/NavBar';
 import Projects from './Projects/Projects';
+import About from './About/About';
 
 function App() {
   const [isHovered, setIsHovered] = useState(false);
@@ -16,22 +16,20 @@ function App() {
         <h2
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
-        ><a  className="Header-name" href='mailto:lekdup.dev@gmail.com'>Tenzin.dev {isHovered ? (
+        ><a  className="Header-name" href='mailto:lekdup.dev@gmail.com'>Tenzin Sivukpa {isHovered ? (
           <FontAwesomeIcon icon={ faEnvelope } bounce/>
         ) : (
           <FontAwesomeIcon icon={ faEnvelope } />
         )}</a></h2>
         <NavBar />
-        <ul className="Header-social">
-          <li><a href='https://www.linkedin.com/in/lekdup/' target='blank' title='LinkedIn' className='Header-social-linkedin'><FontAwesomeIcon icon={faLinkedin} /></a></li>
-          <li><a href='https://github.com/lekdup' target='blank' title='GitHub' className='Header-social-github'><FontAwesomeIcon icon={faGithub} /></a></li>
-          <li><a id="cv" href="https://drive.google.com/file/d/1GZap1Ty7l6peQe_luh8qCJDTkCXqHzY0/view?usp=sharing" target='blank'>Resume <FontAwesomeIcon icon={faDownload} /></a></li>
-        </ul>
       </header>
-      <About />
-      <Projects />
+      <main className="container">
+        <Home />
+        <About />
+        <Projects />
+      </main>
       <footer>
-        <FontAwesomeIcon icon={ faCode } /> and Crafted with <FontAwesomeIcon icon={ faHeart } beat id="heart" /> by <a href="https://linkedin.com/in/lekdup/" target="_blank" rel="noreferrer">Tenzin SIVUKPA </a>
+        <p><FontAwesomeIcon icon={ faCode } /> and Crafted with <FontAwesomeIcon icon={ faHeart } beat id="heart"/> by <a href="https://linkedin.com/in/lekdup/" target="_blank" rel="noreferrer">Tenzin SIVUKPA </a></p>
       </footer>
     </div>
   );
--- my-portfolio/src/components/Projects/Projects.js
+++ my-portfolio/src/components/Projects/Projects.js
@@ -9,29 +9,41 @@ function Projects() {
       <div className="Projects-container">
         <article className='Projects-component'>
           <div className="Projects-component-image">
-            <img src="https://source.unsplash.com/LJMFUzvuau8" alt="Jordan 13 shoes from unsplash source" />
+            <img src="https://source.unsplash.com/AZf7DL-8Qhk" alt="Jordan 13 shoes from unsplash source" />
           </div>
           <div className="Projects-component-summary">
             <h3>oShop - Back Office</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate provident iste aspernatur eos ea sapiente consequatur nostrum assumenda in.</p>
+            <p>oShop, a pivotal project developed during my Bac +2 Web Developer course, showcases my proficiency in PHP and Bootstrap. This dynamic web application serves as an efficient back office solution for catalog managers and administrators of an e-commerce shoe store.</p>
           </div>
         </article>
+
+        <article className='Projects-component'>
+          <div className="Projects-component-image">
+            <img src="https://i.gifer.com/7wC0.gif" alt="Jordan 13 shoes from unsplash source" />
+          </div>
+          <div className="Projects-component-summary">
+            <h3>Weather Widget</h3>
+            <p>The Weather Widget stands as a testament to my growing expertise in React development and API integration. Created with the intention of refining my skills, this widget offers real-time weather updates for different cities by fetching data from external APIs.</p>
+          </div>
+        </article>
+
         <article className='Projects-component'>
           <div className="Projects-component-image">
             <img src="https://source.unsplash.com/QL0FAxaq2z0" alt="Jordan 13 shoes from unsplash source" />
           </div>
           <div className="Projects-component-summary">
-            <h3>oShop - Back Office</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate provident iste aspernatur eos ea sapiente consequatur nostrum assumenda in.</p>
+            <h3>O'Boulot</h3>
+            <p>This innovative online job agency redefines the job search process by empowering candidates to secure employment opportunities without the constraints of qualifications or traditional CVs.</p>
           </div>
         </article>
+
         <article className='Projects-component'>
           <div className="Projects-component-image">
-            <img src="https://source.unsplash.com/w83s82yd3Fk" alt="Jordan 13 shoes from unsplash source" />
+            <img src="https://source.unsplash.com/b21Ty33CqVs" alt="Jordan 13 shoes from unsplash source" />
           </div>
           <div className="Projects-component-summary">
-            <h3>oShop - Back Office</h3>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut voluptate provident iste aspernatur eos ea sapiente consequatur nostrum assumenda in.</p>
+            <h3>Frontend Mentor Projects</h3>
+            <p>My journey into web development started with Frontend Mentor, where I embarked on a series of projects that marked my foray into building static and responsive websites using HTML and CSS.</p>
           </div>
         </article>
       </div>
