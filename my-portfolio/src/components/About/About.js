9a5b498c0f6b2da5b32799e24343d46c5babfbd0
lekdup <lekdup@live.fr>
2023-08-31 18:45:16 +0200

    about me section

--- my-portfolio/src/components/About/About.js
+++ my-portfolio/src/components/About/About.js
@@ -2,10 +2,19 @@ import './About.scss';
 
 function About() {
   return(
-    <p>With a recent graduation from an intensive web development course that earned me a Bac +2 degree, I've laid a solid foundation for my journey into the digital realm. 
-    Eager to delve even deeper, I'm currently pursuing a bachelor's degree in a 'Project Web and Mobile' program at Bac +3 level. 
-    This academic pusuit goes hand in hand with real-world exposure through an internship contract, allowing me to seamlessly bridge theory and practice. 
-    Join me as I weave code into captivating web and mobile experiences, drawing inspiration from the vibrant culture and innovation that Paris ignites within me.</p>
+    <section className="About">
+      <h3 className="About-title">ABOUT ME</h3>
+      <div className="About-detail">
+        <p className='About-detail-text'>
+          With a recent graduation from an intensive web development course that earned me a Bac +2 degree, I've laid a solid foundation for my journey into the digital realm. 
+          Eager to delve even deeper, I'm currently pursuing a bachelor's degree in a 'Project Web and Mobile' program at Bac +3 level. 
+          This academic pusuit goes hand in hand with real-world exposure through an internship contract, allowing me to seamlessly bridge theory and practice. 
+          Join me as I weave code into captivating web and mobile experiences, drawing inspiration from the vibrant culture and innovation that Paris ignites within me.
+        </p>
+      </div>
+
+
+    </section>
   );
 }
 
